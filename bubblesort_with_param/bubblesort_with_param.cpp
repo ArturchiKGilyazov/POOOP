c99129f4888227de733c8c95a7ed37fb3ea259c1
Артур Гилязов <arik2517@outlook.com>
2020-01-22 15:22:17 +0700

    Добавьте файлы проекта.

---
+++ bubblesort_with_param/bubblesort_with_param.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+void swap(int* a, int* b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+template< class Stype> void bubble(Stype* a, int N) {
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N - 1; j++) {
+			if (a[j] > a[j + 1]) swap(a[j], a[j+1]);
+		}
+}
+int main()
+{
+	const int N = 10;
+	int a[N];
+	for (int i = 0; i < N; i++) {
+		a[i] = rand() % 20;
+	}
+	bubble(a, N);
+	for (int i = 0; i < N; i++) {
+		cout << a[i] << " ";
+	}
+}
